607dad6234b30fc6392956d710f2000900b641c6
kero <kero.liao@jcg-tech.com>
2022-02-25 14:30:43 +0800

    update dist

--- chainDialog/vue.config.js
+++ chainDialog/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/archived-project/chainDialog/"
+      ? "/archived-project/chainDialog/dist/"
       : "/",
 };
