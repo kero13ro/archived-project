9b56c32aa83f879b337855651e9e0dd65c4c46d8
kero13ro <32509189+kero13ro@users.noreply.github.com>
2022-08-19 09:58:08 +0800

    update info list

--- chainDialog/src/components/Dialog.vue
+++ chainDialog/src/components/Dialog.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   #dialog
-    h5 彈窗標題
+    h5 Title
     .row
-      Button(@click="$emit('ok')") 確認
-      Button(@click="$emit('cancel')") 取消
+      Button(@click="$emit('ok')") OK
+      Button(@click="$emit('cancel')") Cancel
 
 
 </template>
@@ -16,12 +16,12 @@ export default {};
 #dialog {
   background-color: #eee;
   position: fixed;
-  top: 50vh;
+  top: 55vh;
   left: 50vw;
   transform: translate(-50%, -50%);
 
   border: 1px solid #ddd;
-  padding: 20px;
+  padding: 10px 20px;
   box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.1);
   width: 300px;
   font-weight: bold;
--- chainDialog/src/components/HelloWorld.vue
+++ chainDialog/src/components/HelloWorld.vue
@@ -6,14 +6,17 @@
       | 呼叫彈窗，並可以串連不同按鈕  
       code onOk, onCancel
       | 等回呼函數。
-
-    div 透過類似 jQuery 之閉包封裝作用域，以達到非同步執行不同結果。
     .bg(v-highlight)
       pre
         code(class="javascript") {{code}}
+    ul
+      li 透過類似 jQuery 之閉包封裝作用域，以達到非同步執行不同結果。
+      li 使用 Vue Render Functions 渲染彈窗，不用要在 data 內控制顯示狀態。
 
 
     Button(@click="ShowDailog") 顯示彈窗
+
+    h4 {{resp}}
 
 
 </template>
@@ -22,13 +25,10 @@
 export default {
   data() {
     return {
+      resp: "",
       code: `this.$dialog()
-  .onOk(() => {
-    alert("點擊了確認按鈕");
-  })
-  .onCancel(() => {
-    alert("點擊了取消按鈕");
-  });
+  .onOk(() => { ... })
+  .onCancel(() => { ... });
 `
     };
   },
@@ -39,12 +39,13 @@ export default {
   },
   methods: {
     ShowDailog() {
+      this.resp = ""
       this.$dialog()
         .onOk(() => {
-          alert("點擊了確認");
+          this.resp = "點擊了確認"
         })
         .onCancel(() => {
-          alert("點擊了取消");
+          this.resp = "點擊了取消"
         });
     }
   }
